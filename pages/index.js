352ce58a9943a6a560ad5fcaf851fb90daa5acef
Tyler S <42685071+Tyler-Seliber@users.noreply.github.com>
2020-02-25 21:48:38 -0500

    Added about page

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,14 @@
 // Here's an example of what a sample page looks like.
 // See more: https://nextjs.org/learn/basics/getting-started/first-page
 
-export default () => (
-  <div>
-    <p>Hello DSC!</p>
-  </div>
-)
+import Link from "next/link";
+
+export default function index() {
+  return (
+    <div>
+      <Link href="/about">
+        <p>Hello DSC!</p>
+      </Link>
+    </div>
+  );
+}
